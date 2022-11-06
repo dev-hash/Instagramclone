7294b2c3fab64f7734a8b710f27f57bbcbbf277b
dev-hash <devlovanshi85@gmail.com>
2022-11-06 10:17:55 +0530

    add

--- index.js
+++ index.js
@@ -15,7 +15,42 @@ let getData=async()=>{
 
     let data=await response.json();
 
-    append(data,post_div);
+    //append(data,post_div);
+    createButton(data.length,2);
 }
 
 getData()
+
+let getpaginateData=async(clicked_btn,limit)=>{
+      
+    post_div.innerHTML=null;  
+    let response=await fetch(`http://localhost:3000/posts?_page=${clicked_btn}&_limit=${limit}`);
+
+    let data=await response.json();
+
+    append(data,post_div);
+    //createButton(data.length,2);
+}
+getpaginateData(1,2);
+
+const createButton=(total_images,images_per_page)=>{
+
+   const buttons=Math.ceil(total_images/images_per_page)
+
+    for(let i=1;i<=buttons;i++){
+
+
+        let btn=document.createElement("button");
+
+        btn.id=i;
+        btn.innerText=i;
+        
+
+        btn.onclick=()=>{
+           getpaginateData(i,2);
+        }
+
+        document.getElementById("buttons").append(btn);
+
+    }
+}
